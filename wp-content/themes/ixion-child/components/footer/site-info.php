761b17998a48d1b48b47f29b352f5f69c07a43cc
Thiago Ferreira de Sousa <thiago13115@gmail.com>
2019-08-21 01:53:00 -0300

    Pagina Home finalizada e Pagina Sobre montada

--- wp-content/themes/ixion-child/components/footer/site-info.php
+++ wp-content/themes/ixion-child/components/footer/site-info.php
@@ -1,5 +1,5 @@
 <div class="site-info">
-	<p>&copy; ENCONTRO DE ALUNOS DO COF SP | 2019 - Todos os direitos reservados.</p>
-	<span class="sep"></span>
+	<p>Tema Utilizado: Ixion</p>
 	<p>Desenvolvido por: Thiago Ferreira de Sousa</p>
+	<p>&copy; ENCONTRO DE ALUNOS DO COF SP | 2019 - Todos os direitos reservados.</p>
 </div><!-- .site-info -->
